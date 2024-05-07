b0bdb917fdc06bc496f3c3fb42f371fcc4fa9c99
turgu <power_boy215@windowslive.com>
2024-05-08 00:08:09 +0300

    gri siyah beyaz yaptım

--- turgut.py
+++ turgut.py
@@ -1,7 +1,12 @@
 import inspect
-
+import numpy as np
 def Bebekfurkan(f):
   getattr(__builtins__, f)(inspect.stack()[0][3])
 
 
-Bebekfurkan("print")
+Bebekfurkan("print") #Fonksiyon ismini printliyoru
+def imagegrayscale(image:np.array): #Burası içine koyulan fotoğrafı siyahbeyaz yapai
+  img_rescaled = image / 255
+  grayscale = np.array([0.2126, 0.7152, 0.0722])
+  img_gray = np.matmul(img_rescaled, grayscale)
+  return img_gray
